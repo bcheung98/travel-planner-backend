df7e4e438abfa3720f921500c9023992d3b30a01
Brian <briantcheung@gmail.com>
2021-05-20 13:46:42 -0700

    Fixed a typo in app controller, added pswd confirmation to seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::API
 
     def current_user()
         if decoded_token
-            user_id = decoded_token[0]["user-id"]
+            user_id = decoded_token[0]["user_id"]
             @@user = User.find_by(id: user_id)
         end
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 User.destroy_all
 Destination.destroy_all
 
-User.create(username: "mioakiyama", first_name: "Mio", last_name: "Akiyama", email: "leftybass@email.com", password_digest: "123456")
-User.create(username: "azusanakano", first_name: "Azusa", last_name: "Nakano", email: "azunyan@email.com", password_digest: "123456")
-User.create(username: "yuihirasawa", first_name: "Yui", last_name: "Hirasawa", email: "yui@email.com", password_digest: "123456")
-User.create(username: "ritsutainaka", first_name: "Ritsu", last_name: "Tainaka", email: "buchou@email.com", password_digest: "123456")
-User.create(username: "tsumugikotobuki", first_name: "Tsumugi", last_name: "Kotobuki", email: "takuan@email.com", password_digest: "123456")
+User.create(username: "mioakiyama", first_name: "Mio", last_name: "Akiyama", email: "leftybass@email.com", password: "123456", password_confirmation: "123456")
+User.create(username: "azusanakano", first_name: "Azusa", last_name: "Nakano", email: "azunyan@email.com", password: "123456", password_confirmation: "123456")
+User.create(username: "yuihirasawa", first_name: "Yui", last_name: "Hirasawa", email: "yui@email.com", password: "123456", password_confirmation: "123456")
+User.create(username: "ritsutainaka", first_name: "Ritsu", last_name: "Tainaka", email: "buchou@email.com", password: "123456", password_confirmation: "123456")
+User.create(username: "tsumugikotobuki", first_name: "Tsumugi", last_name: "Kotobuki", email: "takuan@email.com", password: "123456", password_confirmation: "123456")
 
 Destination.create(name: "Eiffel Tower", location: "Paris", country: "France")
 Destination.create(name: "Angkor Wat", location: "Angkor", country: "Cambodia")
