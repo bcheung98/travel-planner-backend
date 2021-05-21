7757ddccfe54f5132580d4f721ba37f08c8f5ea0
Brian <briantcheung@gmail.com>
2021-05-21 15:05:27 -0700

    Added delete destination function

--- app/controllers/user_destinations_controller.rb
+++ app/controllers/user_destinations_controller.rb
@@ -15,6 +15,15 @@ class UserDestinationsController < ApplicationController
         end
     end
 
+    def destroy()
+        user_destination = @@user.user_destinations.find_by(destination_id: params[:id])
+        if user_destination.destroy
+            render json: { success: "Destination removed from list" }, :status => :ok
+        else
+            render json: { error: "Unable to remove destination" }, :status => :bad_request
+        end
+    end
+
     private
     def user_destination_params()
         params.require(:destination).permit(:id)
